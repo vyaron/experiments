5c1d5721273a13e2a6b70ee8ec4c35d8007bb457
Sam Storie <sam.storie@emerson.com>
2016-04-03 08:09:06 -0500

    Added the topshelf-angular2-service code with the most basic topshelf service possible.

---
+++ topshelf-angular2-service/Service/ServiceHost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Service
+{
+    /// <summary>
+    /// The class that provides the actions to perform when starting/stopping the service.
+    /// </summary>
+    public class ServiceHost
+    {
+        public ServiceHost()
+        {
+            Console.WriteLine("ServiceHost constructed");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("ServiceHost started");
+        }
+
+        public void Shutdown()
+        {
+            Console.WriteLine("ServiceHost shutting down");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("ServiceHost stopped");
+        }
+    }
+}
