8a375b43d04aa7e74b1a9bdf3136d58359680b39
Sam Storie <sam.storie@emerson.com>
2016-04-15 07:56:10 -0500

    Updates from writing first draft

--- topshelf-angular2-service-webpack/Service.Website/webpack.config.js
+++ topshelf-angular2-service-webpack/Service.Website/webpack.config.js
@@ -1,36 +1,34 @@
 ﻿
 module.exports = {
+    // Define the entry points for our application so webpack knows what to 
+    //  use as inputs
+    //
     entry: {
         app: ["./App/main"]
     },
+
+    // Define where the resulting file should go
+    //
     output: {
         filename: "assets/[name].bundle.js"
     },
+
     resolve: {
         extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
 
     // Turn on source maps for all applicable files.
-    //  Note, some file types (like sass/css) also require additional annotations
-    //  on their loaders for this to work.
     //
     devtool: "source-map",
 
     module: {
         loaders: [
-            // Process any typescript or typescript-jsx files using the ts-loader first,
-            //  and then pass them through the ng-annotate loader to automatically annotate
-            //  angular related function calls.
+            // Process any typescript or typescript-jsx files using the ts-loader
             //
             {
                 test: /\.tsx?$/,
                 loaders: ["ts-loader"]
             }
-        ],
-
-        noParse: [/zone\.js\/dist\/.+/, /angular2\/bundles\/.+/],
-
-        // we need this due to problems with es6-shim
-        node: { global: "window", progress: false, crypto: "empty", module: false, clearImmediate: false, setImmediate: false }
+        ]
     }
 }
