28c035614463b353aaf86c9fee6ea52faec84254
Sam Storie <storie@gmail.com>
2016-05-20 06:51:54 -0500

    Updated to use the zip code api

--- 0-60-with-angular2-and-visual-studio/source/WeatherHistory.Web/Api/TemperaturesController.cs
+++ 0-60-with-angular2-and-visual-studio/source/WeatherHistory.Web/Api/TemperaturesController.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Deserializers;
 using WeatherHistory.Web.Models;
 
 namespace WeatherHistory.Web.Api
@@ -16,26 +22,73 @@ namespace WeatherHistory.Web.Api
         //  route
         //
         [Route("")]
-        public IHttpActionResult Get(int zipCode)
+        public async Task<IHttpActionResult> Get(string zipCode)
         {
+            // Now we can make our first API call to map the zip code
+            //  to the geo coordinates we need
+            //
+            var zipCodeResponse = await RequestGeoFromZipcode(zipCode);
+
             // To start, just return a list of dummy results to show the API 
             //  endpoint is working
             //
             var today = DateTime.Now;
 
+            // Now our dummy data will actually use the latitude and longitude 
+            //  retrieved from the zip code API
+            //
             var temps = new List<HistoricalTemperature>
             {
-                new HistoricalTemperature {Date = today, Latitude = 44.9513057, Longitude = -93.0916494, High = 85, Low = 55},
-                new HistoricalTemperature {Date = today.AddYears(-1), Latitude = 44.9513057, Longitude = -93.0916494, High = 85, Low = 55},
-                new HistoricalTemperature {Date = today.AddYears(-2), Latitude = 44.9513057, Longitude = -93.0916494, High = 85, Low = 55},
-                new HistoricalTemperature {Date = today.AddYears(-3), Latitude = 44.9513057, Longitude = -93.0916494, High = 85, Low = 55},
-                new HistoricalTemperature {Date = today.AddYears(-4), Latitude = 44.9513057, Longitude = -93.0916494, High = 85, Low = 55}
+                new HistoricalTemperature {Date = today, Latitude = zipCodeResponse.Latitude, Longitude = zipCodeResponse.Longitude, High = 85, Low = 55},
+                new HistoricalTemperature {Date = today.AddYears(-1), Latitude = zipCodeResponse.Latitude, Longitude = zipCodeResponse.Longitude, High = 85, Low = 55},
+                new HistoricalTemperature {Date = today.AddYears(-2), Latitude = zipCodeResponse.Latitude, Longitude = zipCodeResponse.Longitude, High = 85, Low = 55},
+                new HistoricalTemperature {Date = today.AddYears(-3), Latitude = zipCodeResponse.Latitude, Longitude = zipCodeResponse.Longitude, High = 85, Low = 55},
+                new HistoricalTemperature {Date = today.AddYears(-4), Latitude = zipCodeResponse.Latitude, Longitude = zipCodeResponse.Longitude, High = 85, Low = 55}
             };
 
             // There's no real error condition here so just return the 200
             //  response with our List automatically serialized for the caller
             //
             return Ok(temps);
+        }
+
+        private async Task<ZipCodeApiResponse> RequestGeoFromZipcode(string zipcode)
+        {
+            // Create a RestSharp client we can use to make the API call
+            //
+            var client = new RestClient("https://www.zipcodeapi.com");
+
+            // Now build up a request that matches what this API is expecting. We include
+            //  out authentication token and the zipcode right in the URI, and that's simple
+            //  to do with RestSharp's url segments
+            //
+            var request = new RestRequest("/rest/{apiKey}/info.json/{zipcode}/degrees", Method.GET);
+            request.AddUrlSegment("apiKey", ConfigurationManager.AppSettings["zip-code-api-key"]);
+            request.AddUrlSegment("zipcode", zipcode);
+
+            // Now any HTTP call will be asynchronous, but this is trivial to handle with
+            //  the async/await functionality available in .NET
+            //
+            var response = await client.ExecuteTaskAsync(request);
+
+            // Finally, we need to "decode" the JSON data returned so we can load it
+            //  into our internal object.
+            //
+            var content = JObject.Parse(response.Content);
+
+            // Just populate a new object using the key's that existed in the JSON
+            //  data returned
+            //
+            var zipCodeResponse = new ZipCodeApiResponse
+            {
+                Zipcode = Convert.ToString(content["zip_code"]),
+                Latitude = Convert.ToDouble(content["lat"]),
+                Longitude = Convert.ToDouble(content["lng"]),
+                City = Convert.ToString(content["city"]),
+                State = Convert.ToString(content["state"])
+            };
+
+            return zipCodeResponse;
         }
     }
 }
---
+++ 0-60-with-angular2-and-visual-studio/source/WeatherHistory.Web/Models/ZipCodeApiResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherHistory.Web.Models
+{
+    public class ZipCodeApiResponse
+    {
+        public string Zipcode { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+    }
+}
