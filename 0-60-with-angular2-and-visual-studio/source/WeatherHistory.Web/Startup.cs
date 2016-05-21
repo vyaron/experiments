013cbb5e05c3920dc983481b53c7e2dafe54cbc2
Sam Storie <storie@gmail.com>
2016-05-21 07:31:50 -0500

    Updated to use camelCase when serializing JSON

--- 0-60-with-angular2-and-visual-studio/source/WeatherHistory.Web/Startup.cs
+++ 0-60-with-angular2-and-visual-studio/source/WeatherHistory.Web/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Owin;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace WeatherHistory.Web
 {
@@ -13,12 +15,21 @@ namespace WeatherHistory.Web
             //
             appBuilder.Map("/api", api =>
             {
+                // This object is what we use to configure the behavior
+                //  of WebAPI in our application
+                //
                 var httpConfiguration = new HttpConfiguration();
 
                 // We'll use attribute based routing instead of the 
                 //  convention-based approach
                 //
                 httpConfiguration.MapHttpAttributeRoutes();
+
+                // Change the serialization so it does camelCase
+                //
+                var jsonFormatter = httpConfiguration.Formatters.JsonFormatter;
+                var settings = jsonFormatter.SerializerSettings;
+                settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
                 // Now add in web api to the OWIN pipeline
                 //
