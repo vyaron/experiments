e818f96dd91a38a7dc4df3c294c2e47b237ee8be
Sam Storie <storie@gmail.com>
2016-05-22 07:38:59 -0500

    Adding webpack and typings to build application

---
+++ 0-60-with-angular2-and-visual-studio/source/WeatherHistory.Web/webpack.config.js
@@ -0,0 +1,34 @@
+﻿
+module.exports = {
+    // Define the entry points for our application so webpack knows what to 
+    //  use as inputs
+    //
+    entry: {
+        app: ["./App/main"]
+    },
+
+    // Define where the resulting file should go
+    //
+    output: {
+        filename: "content/[name].bundle.js"
+    },
+
+    resolve: {
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+
+    // Turn on source maps for all applicable files.
+    //
+    devtool: "source-map",
+
+    module: {
+        loaders: [
+            // Process any typescript or typescript-jsx files using the ts-loader
+            //
+            {
+                test: /\.tsx?$/,
+                loaders: ["ts-loader"]
+            }
+        ]
+    }
+}
