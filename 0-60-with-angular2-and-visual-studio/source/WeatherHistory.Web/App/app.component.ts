4bc171b9419c921c013b2d793b573730a8c22aef
Sam Storie <storie@gmail.com>
2016-05-22 07:33:02 -0500

    Added tsconfig and the first application files

---
+++ 0-60-with-angular2-and-visual-studio/source/WeatherHistory.Web/App/app.component.ts
@@ -0,0 +1,13 @@
+﻿import {Component} from "@angular/core";
+
+@Component({
+    selector: "my-weather-app",
+    template: `
+        <div>
+            Angular 2 is working!
+        </div>
+    `
+})
+export class AppComponent {
+
+}
---
+++ 0-60-with-angular2-and-visual-studio/source/WeatherHistory.Web/App/main.ts
@@ -0,0 +1,13 @@
+﻿///<reference path="../typings/index.d.ts"/>
+
+import "es6-shim";
+import "reflect-metadata";
+import "zone.js/dist/zone";
+import "zone.js/dist/long-stack-trace-zone";
+
+import {bootstrap} from "@angular/platform-browser-dynamic";
+
+import {AppComponent} from "./app.component";
+
+bootstrap(AppComponent);
+
