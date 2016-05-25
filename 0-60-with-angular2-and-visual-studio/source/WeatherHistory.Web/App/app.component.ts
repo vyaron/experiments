df26c2c5b05fc2bae123b3f0b0013771069a8c26
Sam Storie <storie@gmail.com>
2016-05-24 20:04:15 -0500

    Some final updates as I wrote the blog post

--- 0-60-with-angular2-and-visual-studio/source/WeatherHistory.Web/App/app.component.ts
+++ 0-60-with-angular2-and-visual-studio/source/WeatherHistory.Web/App/app.component.ts
@@ -2,13 +2,21 @@
 import {Http, Headers, Response} from "@angular/http";
 
 import {CHART_DIRECTIVES} from "ng2-charts/ng2-charts";
-    
-import {ZipcodeWeather, ZipcodeWeatherMapper} from "./zipcodeWeather.model";
+
+import {ZipcodeWeather, ZipcodeWeatherMapper, HistoricalTemperature} from "./zipcodeWeather.model";
 
 @Component({
     selector: "my-weather-app",
     directives: [CHART_DIRECTIVES],
-    styles: [ require("./app.styles.scss") ],
+    //
+    // By using the webpack raw-loader in the processing of .scss files we
+    //  can use sass *and* Angular 2's view encapsulation together. Awesome!
+    //
+    styles: [require("./app.styles.scss")],
+    //
+    // By using the webpack html-loader we can leverage Visual Studio's intelli-sense
+    //  for html files
+    //
     template: require("./app.template.html"),
 })
 export class AppComponent {
@@ -39,6 +47,8 @@ export class AppComponent {
         this.zipcodeWeather = undefined;
         this.loading = true;
 
+        // Make the API call using standard Angular 2
+        //
         this.http
             .get(`http://localhost:52588/api/temperatures?zipcode=${this.zipcode}`, {
                 headers: new Headers({
@@ -50,25 +60,32 @@ export class AppComponent {
                 this.loading = false;
                 this.zipcodeWeather = ZipcodeWeatherMapper.fromObject(response.json());
 
+                // let's update the order of the temps regardless of how they came in
+                //  from the API
+                //
+                this.zipcodeWeather.HistoricalTemperatures = this.zipcodeWeather.HistoricalTemperatures
+                    .sort((a: HistoricalTemperature, b: HistoricalTemperature) => {
+                        return a.date > b.date ? 1 : a.date < b.date ? -1 : 0;
+                    });
+
                 // Now map the response into the data we need for the chart
                 //
                 this.barChartLabels = this.zipcodeWeather.HistoricalTemperatures.map(x => x.date.getFullYear().toString());
 
                 this.barChartData = [
                     {
-                        data: this.zipcodeWeather.HistoricalTemperatures.map(x => x.low),
-                        label: "Low"
-                    },
-                    {
                         data: this.zipcodeWeather.HistoricalTemperatures.map(x => x.high),
                         label: "High"
+                    },
+                    {
+                        data: this.zipcodeWeather.HistoricalTemperatures.map(x => x.low),
+                        label: "Low"
                     }
                 ];
             },
             (error: Response) => {
                 this.errorMessage = error.json().message;
                 this.loading = false;
-            }
-            );
+            });
     }
 }
